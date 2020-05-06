311d289c1cc88bc974b7f71661be8596be2897d9
Gi Ppeum, Lee <48782921+pppeum62@users.noreply.github.com>
2020-05-06 18:30:59 +0900

    #7 13-7. 제어문 응용 - ConsoleKey 예제

--- cscondition/Program.cs
+++ cscondition/Program.cs
@@ -98,6 +98,40 @@ namespace cscondition
             {
                 Console.WriteLine("^^");
             }
+
+            ConsoleKeyInfo info = Console.ReadKey();
+            
+            switch(info.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    Console.WriteLine("위로 이동");
+                    break;
+                case ConsoleKey.RightArrow:
+                    Console.WriteLine("오른쪽으로 이동");
+                    break;
+                case ConsoleKey.DownArrow:
+                    Console.WriteLine("아래로 이동");
+                    break;
+                case ConsoleKey.LeftArrow:
+                    Console.WriteLine("왼쪽으로 이동");
+                    break;
+                default:
+                    switch(info.KeyChar)
+                    {
+                        /*
+                        case (char)38:
+                            Console.WriteLine("위로 이동");
+                            break;
+                            */
+                        case '가':
+                            Console.WriteLine("가 입력");
+                            break;
+                        default:
+                            Console.WriteLine("방향키가 아닌 키를 눌렀습니다");
+                            break;
+                    }
+                    break;
+            }
         }
     }
 }
