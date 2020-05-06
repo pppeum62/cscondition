7f33cd77b2f374816e630505c0f759935cd5705a
Gi Ppeum, Lee <48782921+pppeum62@users.noreply.github.com>
2020-05-06 17:53:54 +0900

    #2 13-4. if else if 조건문 실습

--- cscondition/Program.cs
+++ cscondition/Program.cs
@@ -33,15 +33,12 @@ namespace cscondition
             if(DateTime.Now.Hour < 11)
             {
                 Console.WriteLine("아침 먹을 시간입니다");
+            } else if(DateTime.Now.Hour < 15)
+            {
+                Console.WriteLine("점심 먹을 시간입니다.");
             } else
             {
-                if(DateTime.Now.Hour < 15)
-                {
-                    Console.WriteLine("점심 먹을 시간입니다.");
-                } else
-                {
-                    Console.WriteLine("저녁 먹을 시간입니다.");
-                }
+                Console.WriteLine("저녁 먹을 시간입니다.");
             }
         }
     }
